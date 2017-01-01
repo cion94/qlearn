beb37b89cdba980a89b2fe801b32168ac95bc95a
cion94 <cristian.ion94@gmail.com>
2017-01-01 17:33:07 +0200

    dql skip frames

--- breakout.py
+++ breakout.py
@@ -1,9 +1,12 @@
 import json
+import random
+
 import pygame, sys, math
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Convolution2D, Flatten
 from keras.models import model_from_json
+from skimage.color import rgb2gray
 
 black = (0, 0, 0)
 red = (255, 0, 0)
@@ -12,121 +15,81 @@ white = (255, 255, 255)
 
 
 class QLearning:
-    def __init__(self, epsilon=0.1, frames=4, max_memory=4096, batch_size=128, num_actions=3, discount=0.9):
-        self.epsilon = epsilon
-        self.frames = frames
-        self.max_memory = max_memory
-        self.batch_size = batch_size
+    def __init__(self, num_actions=3):
+        self.epsilon = 1.0
+        self.train_timestep = 4
+        self.random_replay_frames = 20000
+        self.max_memory = 100000
+        self.batch_size = 32
         self.model = None
         self.num_actions = num_actions
         self.memory = []
-        self.discount = discount
+        self.discount = 0.99
         self.loss = 0
         self.conv_model()
+        self.t = 0
 
     def conv_model(self):
         self.model = Sequential()
-        self.model.add(Convolution2D(32, 8, 8, subsample=(4, 4), dim_ordering='th', input_shape=(self.frames, 84, 84),
-                                     border_mode='valid'))
+        self.model.add(Convolution2D(32, 8, 8, subsample=(4, 4), input_shape=(4, 84, 84), dim_ordering='th'))
         self.model.add(Activation("relu"))
-        self.model.add(Convolution2D(64, 4, 4, subsample=(2, 2), dim_ordering='th', input_shape=(32, 20, 20), border_mode='valid'))
-        self.model.add(Activation("relu"))
-        self.model.add(Convolution2D(64, 3, 3, dim_ordering='th', input_shape=(64, 9, 9), border_mode='valid'))
-        self.model.add(Activation("relu"))
+        self.model.add(Convolution2D(64, 4, 4, subsample=(2, 2), activation='relu'))
+        self.model.add(Convolution2D(64, 3, 3, subsample=(1, 1), activation='relu'))
         self.model.add(Flatten())
-        self.model.add(Dense(512, input_dim=3136))
-        self.model.add(Activation("relu"))
+        self.model.add(Dense(512, activation='relu'))
         self.model.add(Dense(self.num_actions))
-        self.model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-        # self.model.compile(loss='mse', optimizer='sgd')
+        self.model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
 
     def get_action(self, state):
-        if np.random.rand() <= self.epsilon:
+        if np.random.rand() <= self.epsilon or self.t < self.random_replay_frames:
             action = np.random.randint(0, self.num_actions, size=1)
         else:
-            q = self.model.predict(state)
+            q = self.model.predict(np.reshape(state, (1, 4, 84, 84)))
             action = np.argmax(q[0])
-            print(q, action)
+
+        if self.epsilon > 0.1 and self.t >= self.random_replay_frames:
+            self.epsilon -= 9e-7
+
         return action
 
     def replay(self, data):
-        if len(self.memory) + 1 < self.max_memory:
-            self.memory.append(data)
-        else:
+        if len(self.memory) + 1 > self.max_memory:
             del self.memory[0]
-            self.memory.append(data)
+        self.memory.append(data)
 
     def get_batch(self):
         memory_size = len(self.memory)
         batch_indices = np.random.randint(0, memory_size, self.batch_size)
-        inputs = np.asarray([self.memory[i][0] for i in batch_indices])
+        inputs = np.float32(np.array([self.memory[i][0] for i in batch_indices]) / 255.0)
 
         targets = np.zeros((self.batch_size, self.num_actions))
         for i, idx in enumerate(batch_indices):
             state, action, reward, new_state, game_over = self.memory[idx]
-            targets[i] = self.model.predict(state)[0]
-            q = np.max(self.model.predict(new_state)[0])
+            targets[i] = self.model.predict(np.float32(np.reshape(state, (1, 4, 84, 84)) / 255.0))[0]
+            q = np.max(self.model.predict(np.float32(np.reshape(new_state, (1, 4, 84, 84)) / 255.0))[0])
             if game_over:
                 targets[i, action] = reward
             else:
                 targets[i, action] = reward + self.discount * q
-        inputs = np.reshape(inputs, [self.batch_size, 4, 84, 84])
+        # inputs = np.reshape(inputs, [self.batch_size, 4, 84, 84])
         return inputs, targets
 
-    def train(self):
-        print("train")
-        for e in range(10):
-            inputs, targets = self.get_batch()
-            self.loss = self.model.train_on_batch(inputs, targets)
+    def train(self, data):
+        self.replay(data)
+
+        if self.t >= self.random_replay_frames:
+            if self.t % self.train_timestep == 0:
+                inputs, targets = self.get_batch()
+                self.loss = self.model.train_on_batch(inputs, targets)
+            if self.t % 300000 == 0:
+                self.model.save_weights("model.h5", overwrite=True)
+                with open("model.json", "w") as outfile:
+                    json.dump(self.model.to_json(), outfile)
+
+        self.t += 1
 
 
 class BreakoutEnv:
-    def __init__(self, train=False):
-        self.train = train
-        self.w, self.h = 640, 480
-        pw = 0.15 * self.w
-        ph = 0.05 * self.h
-        bw = self.w / 16
-        bh = 16
-        pygame.init()
-        self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((self.w, self.h), 0, 32)
-        pygame.display.set_caption('Breakout')
-        pygame.mouse.set_visible(0)
-        # game objects
-        self.speed = 6
-        self.radius = 6
-        self.score = dict()
-        self.entities_initial = [[i * bw, (j + 5) * bh, bw, bh] for i in range(16) for j in range(6)]
-        self.ball = [0, 0]
-        self.ball_initial = (int(self.w / 2 - pw / 2), int(self.h / 2 - ph / 2))
-        self.ball_vel = [0, 0]
-        ball_surface = pygame.Surface((2 * self.radius, 2 * self.radius))
-        ball_surface.fill(black)
-        ball_surface.set_colorkey(black)
-        pygame.draw.circle(ball_surface, (255, 255, 255), (self.radius, self.radius), self.radius)
-        ball_surface.set_alpha(50)
-        self.ball_surface = ball_surface
-        self.paddle_initial = (self.w / 2 - pw / 2, self.h - ph, pw, ph)
-        self.paddle = [0, 0, 0, 0]
-        self.prev = [0, 0]
-        self.entities = None
-        self.timer = 0
-        self.start_ticks = 0
-        self.pixel_state = pygame.Surface((84, 84))
-        self.surf = None
-
-    def initialise(self):
-        self.score["lives"] = 3
-        self.score["score"] = 0
-        self.start_ticks = pygame.time.get_ticks()
-        self.paddle = list(self.paddle_initial)
-        self.ball = list(self.ball_initial)
-        self.ball_vel = [1, 2]
-        bw = self.w / 16
-        bh = 16
-        self.entities = [[i * bw, (j + 5) * bh, bw, bh] for i in range(16) for j in range(6)]
-
     def change(self):
         last_score = self.score["score"]
         last_lives = self.score["lives"]
@@ -136,79 +99,62 @@ class BreakoutEnv:
         return last_score, last_lives, seconds
 
     def observe(self, score, lives, seconds):
-        reward = (self.score["score"] - score) * 9
-        reward += (self.score["lives"] - lives) * 10
-        if (self.score["lives"] - lives) == 0:
-            reward += 1
-        reward += seconds * 0.1
+        reward = 0
+        if self.score["score"] - score > 0:
+            reward = 1
+        if self.score["lives"] - lives < 0:
+            reward = -1
         game_over = False
         if self.score["score"] >= len(self.entities_initial):
-            reward += 10
+            reward = 1
             game_over = True
         if self.score["lives"] <= 0:
-            reward -= 300
+            reward = -1
             game_over = True
         return reward, game_over
+
+    def init_state_frame(self):
+        pygame.transform.scale(self.screen, (84, 84), self.pixel_state)
+        arr = pygame.surfarray.pixels3d(self.pixel_state)
+        processed_observation = np.uint8(rgb2gray(arr) * 255)
+        state = [processed_observation for _ in range(4)]
+        return np.stack(state, axis=0)
 
     def get_frame(self):
         pygame.transform.scale(self.screen, (84, 84), self.pixel_state)
         arr = pygame.surfarray.pixels3d(self.pixel_state)
-        avgs = [[(r * 0.298 + g * 0.587 + b * 0.114) for (r, g, b) in col] for col in arr]
-        arr = np.array([[avg for avg in col] for col in avgs])
-        arr2 = np.array([[[avg, avg, avg] for avg in col] for col in avgs])
-        self.surf = pygame.surfarray.make_surface(arr2)
-        return np.reshape(arr, [84, 84])
+        arr = np.uint8(rgb2gray(arr) * 255)
+        return np.reshape(arr, (1, 84, 84))
 
-    def play(self):
-        self.initialise()
-        agent = QLearning(epsilon=0.0, frames=1)
-        with open("model.json", "r") as jfile:
-            agent.model = model_from_json(json.load(jfile))
-        agent.model.load_weights("model.h5")
-        agent.model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-
-        game_over = False
-        s = self.get_state()
-        while not game_over:
-            action = agent.get_action(s)
-            s_new = self.get_state(action)
-            pygame.display.update()
-            s = s_new
-
-    def get_state(self, action=None):
-        state = []
-        for i in range(4):
-            self.step(action)
-            state.append(self.get_frame())
-        state = np.asarray(state)
-        return np.reshape(state, (1, 4, 84, 84))
-
-    def start(self, epochs=1000):
-        self.initialise()
-        agent = QLearning(epsilon=0.2, frames=4)
-
-        for e in range(epochs):
+    def train_agent(self):
+        agent = QLearning()
+        for e in range(12000):
             game_over = False
             self.initialise()
-            state = self.get_state()
-            # agent.memory = []
+            state = self.init_state_frame()
             while not game_over:
                 score, lives, seconds = self.change()
                 action = agent.get_action(state)
-                next_state = self.get_state(action)
+                [self.step(action) for _ in range(4)]
                 reward, game_over = self.observe(score, lives, seconds)
-                agent.replay([state, action, reward, next_state, game_over])
-                if self.surf is not None:
-                    self.screen.blit(self.surf, (0, 0))
-                pygame.display.update()
+                next_state = self.get_frame()
+                next_state = np.append(state[1:, :, :], next_state, axis=0)
+                agent.train((state, action, reward, next_state, game_over))
                 state = next_state
-            agent.train()
-            agent.epsilon -= 0.01
-            print('epoch {} loss {} memory {}'.format(e, agent.loss, len(agent.memory)))
-            # Save trained model weights and architecture, this will be used by the visualization code
-            agent.model.save_weights("model.h5", overwrite=True)
-            with open("model.json", "w") as outfile:
-                json.dump(agent.model.to_json(), outfile)
+
+    def play(self):
+        self.initialise()
+        with open("model.json", "r") as jfile:
+            model = model_from_json(json.load(jfile))
+        model.load_weights("model.h5")
+        model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
+        game_over = False
+        s = self.get_frame()
+        while not game_over:
+            q = model.predict(s)
+            action = np.argmax(q[0])
+            self.k_steps(action)
+            s = self.get_frame()
 
     def step(self, action=None):
         # msElapsed = self.clock.tick(144)
@@ -217,75 +163,41 @@ class BreakoutEnv:
                 pygame.quit()
                 sys.exit()
 
-        self.screen.fill(black)
+        if action == 0:
+            self.paddle[0] -= self.speed
+            self.paddle[0] = max(0, self.paddle[0])
+        if action == 2:
+            self.paddle[0] += self.speed
+            self.paddle[0] = min(self.w - self.paddle[2], self.paddle[0])
 
-        keys = pygame.key.get_pressed()
-        if action is None:
-            if keys[pygame.K_LEFT]:
-                self.paddle[0] -= self.speed
-                self.paddle[0] = max(0, self.paddle[0])
-            if keys[pygame.K_RIGHT]:
-                self.paddle[0] += self.speed
-                self.paddle[0] = min(self.w - self.paddle[2], self.paddle[0])
-        else:
-            if action == 0:
-                self.paddle[0] -= self.speed
-                self.paddle[0] = max(0, self.paddle[0])
-            if action == 2:
-                self.paddle[0] += self.speed
-                self.paddle[0] = min(self.w - self.paddle[2], self.paddle[0])
-
-        # GAME OVER???
-        if self.train is False:
-            if self.score["lives"] <= 0:
-                myfont2 = pygame.font.SysFont("monospace", 35, 1)
-                label = myfont2.render("GAME OVER!", 1, (255, 0, 0))
-                self.screen.blit(label, (self.w / 2 - label.get_width() / 2, self.h / 2 - label.get_height() / 2))
-
-                myfont = pygame.font.SysFont("monospace", 15)
-                label = myfont.render("Press RETURN to try again", 1, (255, 255, 0))
-                self.screen.blit(label, (self.w / 2 - label.get_width() / 2, self.h / 2 - label.get_height() / 2 + 30))
-                if keys[pygame.K_RETURN]:
-                    self.initialise()
-            else:
-                self.update()
+        if self.score["lives"] == 0:
+            self.initialise()
         else:
             self.update()
 
-        pygame.draw.rect(self.screen, red, self.paddle, 0)
+        self.draw()
 
+    def draw(self):
+        self.screen.fill(black)
+        pygame.draw.rect(self.screen, red, self.paddle, 0)
         for shape in self.entities:
             pygame.draw.rect(self.screen, blue, shape, 0)
             pygame.draw.rect(self.screen, black, shape, 1)
-
-        new_ball = [int(i) for i in self.ball]
-        magnitude = math.sqrt(self.ball_vel[0] * self.ball_vel[0] + self.ball_vel[1] * self.ball_vel[1])
-        vel = [-v / magnitude for v in self.ball_vel]
-        for i in range(int(np.abs(self.ball_vel[0]) + np.abs(self.ball_vel[1]))):
-            prev_ball = (new_ball[0] + 2 * i * vel[0], new_ball[1] + 2 * i * vel[1])
-            self.screen.blit(self.ball_surface, (prev_ball[0] - self.radius, prev_ball[1] - self.radius))
+        new_ball = [int(self.ball[0]), int(self.ball[1])]
         pygame.draw.circle(self.screen, white, new_ball, self.radius)
-
+        pygame.display.update()
         # write the score and lives
-        myfont = pygame.font.SysFont("monospace", 15)
+        '''myfont = pygame.font.SysFont("monospace", 15)
         label = myfont.render("Score:" + str(self.score["score"]), 1, (255, 255, 0))
         self.screen.blit(label, (0, 0))
         label = myfont.render("Lives:" + str(self.score["lives"]), 1, (255, 255, 0))
-        self.screen.blit(label, (100, 0))
+        self.screen.blit(label, (100, 0))'''
 
     def update(self):
-        self.prev = [i for i in self.ball]
-        self.ball[0] += self.ball_vel[0]
-        self.ball[1] += self.ball_vel[1]
+        self.prev = np.array(self.ball)
+        self.ball += self.ball_vel
         self.ball_collision()
-        if self.ball_vel[0] > 4:
-            self.ball_vel[0] = 4
-        if self.ball_vel[1] > 4:
-            self.ball_vel[1] = 4
-        if self.ball_vel[0] < -4:
-            self.ball_vel[0] = -4
-        if self.ball_vel[1] < -4:
-            self.ball_vel[1] = -4
+        self.ball_vel = np.clip(self.ball_vel, -4, 4)
 
     def ball_collision(self):
         ball = self.ball
@@ -298,7 +210,7 @@ class BreakoutEnv:
             ball_vel[1] *= -1
             return
         if ball[1] > self.h:
-            self.ball_vel = [1, 2]
+            self.ball_vel = np.array([1, 2])
             self.ball = list(self.ball_initial)
             self.paddle = list(self.paddle_initial)
             self.score["lives"] -= 1
@@ -353,7 +265,53 @@ class BreakoutEnv:
                 brick[0] = self.w + 100
                 self.score["score"] += 1
 
+    def __init__(self, train=False):
+        self.train = train
+        self.w, self.h = 640, 480
+        pw = 0.15 * self.w
+        ph = 0.05 * self.h
+        bw = self.w / 16
+        bh = 16
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.w, self.h), 0, 32)
+        pygame.display.set_caption('Breakout')
+        pygame.mouse.set_visible(0)
+        # game objects
+        self.speed = 6
+        self.radius = 6
+        self.score = dict()
+        self.entities_initial = [[i * bw, (j + 5) * bh, bw, bh] for i in range(16) for j in range(6)]
+        self.ball = np.array([0, 0])
+        self.ball_initial = (int(self.w / 2 - pw / 2), int(self.h / 2 - ph / 2))
+        self.ball_vel = np.array([0, 0])
+        ball_surface = pygame.Surface((2 * self.radius, 2 * self.radius))
+        ball_surface.fill(black)
+        ball_surface.set_colorkey(black)
+        pygame.draw.circle(ball_surface, (255, 255, 255), (self.radius, self.radius), self.radius)
+        ball_surface.set_alpha(50)
+        self.ball_surface = ball_surface
+        self.paddle_initial = (self.w / 2 - pw / 2, self.h - ph, pw, ph)
+        self.paddle = [0, 0, 0, 0]
+        self.prev = np.array([0, 0])
+        self.entities = None
+        self.timer = 0
+        self.start_ticks = 0
+        self.pixel_state = pygame.Surface((84, 84))
+        self.surf = None
+
+    def initialise(self):
+        self.score["lives"] = 3
+        self.score["score"] = 0
+        self.start_ticks = pygame.time.get_ticks()
+        self.paddle = list(self.paddle_initial)
+        self.ball = np.array(self.ball_initial)
+        self.ball_vel = np.array([random.randint(-1, 1), 2])
+        bw = self.w / 16
+        bh = 16
+        self.entities = [[i * bw, (j + 5) * bh, bw, bh] for i in range(16) for j in range(6)]
+
 
 if __name__ == "__main__":
     br = BreakoutEnv(train=True)
-    br.start()
+    br.train_agent()
